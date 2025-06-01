3cdc637678e51f8d94bd4290e866ce2d88b09e59
CTO <cto@CNGCNGH20196966.local>
2025-06-01 21:59:18 +0700

    fix

--- apps/backend/src/modules/my-auth/service.ts
+++ apps/backend/src/modules/my-auth/service.ts
@@ -139,6 +139,7 @@ class MyAuthProviderService extends AbstractAuthModuleProvider {
         })
 
       // console.log('Token Response:', response)
+      // console.log('Token Response:', response)
 
       const { authIdentity, success } = await this.verify_(
         response as TokenResponse,
